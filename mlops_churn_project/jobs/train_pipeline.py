ac85691ddd2600429b56565b93364880af8b4209
Sreejith-tiger <sreejith.marath@tigeranalytics.com>
2025-06-08 14:54:02 +0000

    path issue resolved

--- mlops_churn_project/jobs/train_pipeline.py
+++ mlops_churn_project/jobs/train_pipeline.py
@@ -1,6 +1,12 @@
+import sys
+
 from src.data_preparation import load_data, prepare_data
 from src.evaluate_model import report
 from src.train_model import train
+
+# Dynamically add project root to sys.path
+ROOT = Path(__file__).resolve().parents[1]
+sys.path.insert(0, str(ROOT))
 
 df_raw = load_data()
 df = prepare_data(df_raw)
